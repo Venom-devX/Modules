4fd7da322703a1f50ef6dd160fafb186fbc57e44
Venom DevZ <davilfluna@gmail.com>
2025-06-09 17:14:43 -0300

    Hdoskxbdidhxhxh

--- Universal/ModulesHost.lua
+++ Universal/ModulesHost.lua
@@ -1,4 +1,4 @@
 return {
 	Modules = "https://raw.githubusercontent.com/Venom-devX/Modules/main/Universal/",
-	Scripts = "https://raw.githubusercontent.com/Venom-devX/Sources/main/Universal/"
+	Scripts = "https://raw.githubusercontent.com/Venom-devX/Sources/main/"
 }
